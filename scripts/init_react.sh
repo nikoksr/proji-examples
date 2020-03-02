5e307fc9c48b59fd886414428c57aa4ce9b3e01f
Niko Köser <koeserniko@gmail.com>
2020-03-02 01:28:47 +0100

    docs: remove classes, files and scripts folders

--- scripts/init_react.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters"
-fi
-
-create-react-app .
-npm install --save-dev prettier-eslint eslint-config-prettier
-eslint --init
